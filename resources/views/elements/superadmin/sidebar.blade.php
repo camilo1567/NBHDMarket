e6fe8914fba309866b4bc97fbbafde57b53ba64b
CamiloHorta <camilo20113@hotmail.com>
2023-09-06 21:34:55 -0500

    composer laravel excel, ajuste css sidebar

--- resources/views/elements/superadmin/sidebar.blade.php
+++ resources/views/elements/superadmin/sidebar.blade.php
@@ -12,7 +12,7 @@
                 </a>
             </li>
             <li>
-                <a href="{{ route('dashboard') }}" class="sidebar-item">
+                <a href="{{ route('superadmin.dashboard') }}" class="{{ request()->routeIs('superadmin.dashboard') ? 'sidebar-item-used' : 'sidebar-item' }}">
                     <svg class="sidebar-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                         <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
                         <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
@@ -22,7 +22,7 @@
             </li>
             <li>
                 <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
-                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                    <svg class="sidebar-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                         <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
                     </svg>
                     <span class="flex-1 ml-3 text-left whitespace-nowrap">Usuarios</span>
@@ -30,20 +30,24 @@
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                     </svg>
                 </button>
-                <ul id="dropdown-example" class="hidden py-2 space-y-2">
+                <ul id="dropdown-example" class="{{ request()->routeIs([
+                        'superadmin.users.index',
+                        'superadmin.users.clientes',
+                        'superadmin.users.negocios'
+                    ]) ? 'block' : 'hidden' }} py-2 space-y-2">
                         <li>
-                        <a href="{{ route('superadmin.users.index') }}" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">General</a>
+                            <a href="{{ route('superadmin.users.index') }}" class="{{ request()->routeIs('superadmin.users.index') ? 'sidebar-dropdown-icon-used' : 'sidebar-dropdown-icon' }}">General</a>
                         </li>
                         <li>
-                        <a href="{{ route('superadmin.users.clientes') }}" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Clientes</a>
+                            <a href="{{ route('superadmin.users.clientes') }}" class="{{ request()->routeIs('superadmin.users.clientes') ? 'sidebar-dropdown-icon-used' : 'sidebar-dropdown-icon' }}">Clientes</a>
                         </li>
                         <li>
-                        <a href="{{ route('superadmin.users.negocios') }}" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Negocios</a>
+                            <a href="{{ route('superadmin.users.negocios') }}" class="{{ request()->routeIs('superadmin.users.negocios') ? 'sidebar-dropdown-icon-used' : 'sidebar-dropdown-icon' }}">Negocios</a>
                         </li>
                 </ul>
             </li>
             <li>
-                <a href="{{ route('superadmin.categories.index') }}" class="sidebar-item">
+                <a href="{{ route('superadmin.categories.index') }}" class="{{ request()->routeIs('superadmin.categories.index') ? 'sidebar-item-used' : 'sidebar-item' }}">
                     <svg class="sidebar-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                         <path fill-rule="evenodd" d="M5.25 2.25a3 3 0 00-3 3v4.318a3 3 0 00.879 2.121l9.58 9.581c.92.92 2.39 1.186 3.548.428a18.849 18.849 0 005.441-5.44c.758-1.16.492-2.629-.428-3.548l-9.58-9.581a3 3 0 00-2.122-.879H5.25zM6.375 7.5a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z" clip-rule="evenodd" />
                     </svg>
@@ -51,7 +55,7 @@
                 </a>
             </li>
             <li>
-                <a href="{{ route('superadmin.publicities.index') }}" class="sidebar-item">
+                <a href="{{ route('superadmin.publicities.index') }}" class="{{ request()->routeIs('superadmin.publicities.index') ? 'sidebar-item-used' : 'sidebar-item' }}">
                     <svg  class="sidebar-icon" xmlns="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                         <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
                     </svg>
@@ -59,7 +63,7 @@
                 </a>
             </li>
             <li>
-                <a href="{{ route('superadmin.tickets.index') }}" class="sidebar-item">
+                <a href="{{ route('superadmin.tickets.index') }}" class="{{ request()->routeIs('superadmin.tickets.index') ? 'sidebar-item-used' : 'sidebar-item' }}">
                     <svg class="sidebar-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
                     </svg>
@@ -71,7 +75,7 @@
                 </a>
             </li>
             <li>
-                <a href="{{ route('superadmin.auditoria.index') }}" class="sidebar-item">
+                <a href="{{ route('superadmin.auditoria.index') }}" class="{{ request()->routeIs('superadmin.auditoria.index') ? 'sidebar-item-used' : 'sidebar-item' }}">
                     <svg class="sidebar-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                         <path d="M11.625 16.5a1.875 1.875 0 100-3.75 1.875 1.875 0 000 3.75z" />
                         <path fill-rule="evenodd" d="M5.625 1.5H9a3.75 3.75 0 013.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 013.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 01-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875zm6 16.5c.66 0 1.277-.19 1.797-.518l1.048 1.048a.75.75 0 001.06-1.06l-1.047-1.048A3.375 3.375 0 1011.625 18z" clip-rule="evenodd" />
@@ -82,7 +86,7 @@
                 </a>
             </li>
             <li>
-                <a href="{{ route('superadmin.ajustes') }}" class="sidebar-item">
+                <a href="{{ route('superadmin.ajustes') }}" class="{{ request()->routeIs('superadmin.ajustes') ? 'sidebar-item-used' : 'sidebar-item' }}">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="flex-shrink-0 sidebar-icon">
                         <path fill-rule="evenodd" d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 00-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 00-2.282.819l-.922 1.597a1.875 1.875 0 00.432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 000 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 00-.432 2.385l.922 1.597a1.875 1.875 0 002.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 002.28-.819l.923-1.597a1.875 1.875 0 00-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 000-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 00-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 00-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 00-1.85-1.567h-1.843zM12 15.75a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5z" clip-rule="evenodd" />
                     </svg>
