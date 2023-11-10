5af3172e5f8fe43e5584cf0cbc7c606829fa3df8
MagWeb27 <miguelmartinezgomez27@gmail.com>
2023-11-10 16:57:33 -0500

    rediseño CARD index productos

--- resources/views/negocio/products/index.blade.php
+++ resources/views/negocio/products/index.blade.php
@@ -24,10 +24,10 @@
                 <div
                     class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                     <a href="{{ route('negocio.products.edit', $product) }}" class="flex justify-center">
-                        <img class="rounded-t-lg h-[250px]" src="{{ asset('storage/' . $product->imagen) }}"
+                        <img class="h-[250px] mt-4" src="{{ asset('storage/' . $product->imagen) }}"
                             alt="{{ $product->nombre }}" />
                     </a>
-
+                    <hr class="mt-4">
                     <div class="p-5">
                         <div>
                             <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -48,7 +48,7 @@
                                     </div>
                                 @endif
                             </div>
-                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                            <p class="mb-3 font-normal text-gray-600 dark:text-gray-400">
                                 {{ $product->descripcion }}
                             </p>
                         </div>
