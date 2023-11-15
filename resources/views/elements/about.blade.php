37b34b86773e7e070529fb680d7aa5a6d010bcf8
MagWeb27 <miguelmartinezgomez27@gmail.com>
2023-11-14 20:02:40 -0500

    Diseño act de seccion about

--- resources/views/elements/about.blade.php
+++ resources/views/elements/about.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
     <div class="container mx-auto p-8 w-[90%]">
-        <div class="mb-10">
+        <div class="mb-10 grid gap-4 ">
             <!-- Sección "Sobre Nosotros" -->
             <h1 class="text-3xl text-start font-bold mb-4 pb-4">Sobre Nosotros</h1>
 
-            <section
-                class="mx-auto p-6 mt-10 mb-4 w-full bg-white rounded-lg shadow-lg text-gray-700 hover:text-white hover:bg-blue-700 hover:-translate-y-2 transition duration-300">
+            <div
+                class="bg-white rounded-lg text-gray-700 hover:text-white shadow-lg hover:bg-blue-700 hover:-translate-y-2 transition duration-300 p-6">
 
                 <h1 class="text-3xl text-center font-bold pb-4 drop-shadow-sm">Nuestro slogan</h1>
 
@@ -17,10 +17,6 @@
                     a alcanzar su máximo potencial y donde los consumidores descubran
                     productos y servicios únicos que enriquezcan sus vidas.
                 </p>
-            </section>
-
-            <div
-                class="bg-white rounded-lg text-gray-700 hover:text-white shadow-lg hover:bg-blue-700 hover:-translate-y-2 transition duration-300 p-6">
 
                 <!-- Subsección "Nuestra misión" -->
 
@@ -46,6 +42,35 @@
                     la innovación y la pasión se unan para dar forma a un futuro
                     lleno de oportunidades ilimitadas.
                 </p>
+            </div>
+
+            <!-- Side Section -->
+
+            <div
+                class="h-[500px] bg-white rounded-lg text-gray-700 hover:text-white shadow-lg hover:bg-blue-700 hover:-translate-y-2 transition duration-300 p-6">
+
+                <h1 class="text-3xl text-center font-bold pb-4 drop-shadow-sm">Contactanos</h1>
+
+                <div class="grid grid-cols-4 items-center h-[250px] p-4 gap-8 text-center">
+                    
+                        <div class="flex flex-col  items-center pb-6">
+                            <div class="rounded-full h-[140px] w-[140px] bg-violet-700 border-white mb-2"></div>
+                            <h1 class="text-3xl font-bold">Juan Camilo Horta</h1>
+                        </div>
+                    
+                    <div class="flex flex-col items-center">
+                        <div class="rounded-full h-[140px] w-[140px] bg-violet-700 border-white mb-2"></div>
+                        <h1 class="text-3xl font-bold">Miguel Angel Martinez Gomez</h1>
+                    </div>
+                    <div class="flex flex-col items-center">
+                        <div class="rounded-full h-[140px] w-[140px] bg-violet-700 border-white mb-2"></div>
+                        <h1 class="text-3xl font-bold">Miguel Angel Usma Mosquera</h1>
+                    </div>
+                    <div class="flex flex-col items-center">
+                        <div class="rounded-full h-[140px] w-[140px] bg-violet-700 border-white mb-2"></div>
+                        <h1 class="text-3xl font-bold">Angel Yesid Campiño Vidal</h1>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
